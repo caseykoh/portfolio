a46374111e29702ad984a91e371fce467a120747
caseykoh <s.casey.koh@gmail.com>
2024-04-08 11:03:47 -0400

    Initial tailwind use

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import About from "./components/About/About";
 import Home from "./components/Home/Home";
 import Navbar from "./components/Navbar/Navbar";
 
@@ -8,6 +7,7 @@ function App() {
   return (
     <>
       <Navbar />
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,5 +1,9 @@
 const Home = () => {
-  return <h1>Home</h1>;
+  return (
+    <div className="container min-h-screen bg-gradient-to-br from-slate-950 to-slate-800">
+      <h1 className="font-bold">Home</h1>
+    </div>
+  );
 };
 
 export default Home;

e1de7a9e19a9ed0762cf2a7baaab1eaaef94f3d5
Casey Koh <s.casey.koh@gmail.com>
2024-06-20 10:10:59 -0400

    Removed navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./components/Home/Home";
-import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <>
-      {/* <Navbar /> */}
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
